5b8282bc59526fa9ce31428733253322418dd85a
Aissa Boukraa "Bejaia <Aissa.Bo@Techno-dz.com>
2026-03-11 14:43:41 +0100

    Mise à jour du projet

--- lib/utils/excel_utils.dart
+++ lib/utils/excel_utils.dart
@@ -1,17 +1,73 @@
-import 'package:excel/excel.dart';
 import 'dart:io';
+
+import 'package:excel/excel.dart';
 import 'package:path_provider/path_provider.dart';
 
-Future<void> exportToExcel() async {
-  var excel = Excel.createExcel();
-  var sheet = excel['Historique'];
+class InventoryExportRow {
+  final String code;
+  final String designation;
+  final String barcode;
+  final int quantite;
+  final DateTime date;
 
-  // Ajout de données fictives
-  sheet.appendRow(['Code', 'Désignation', 'Code-barres', 'Quantité', 'Date']);
-  sheet.appendRow(['P123', 'Produit A', '123456789', '10', DateTime.now().toString()]);
+  InventoryExportRow({
+    required this.code,
+    required this.designation,
+    required this.barcode,
+    required this.quantite,
+    required this.date,
+  });
+}
 
-  var fileBytes = excel.encode();
+Future<File> exportInventoryToExcel(List<InventoryExportRow> rows) async {
+  final excel = Excel.createExcel();
+
+  final historySheet = excel['Historique'];
+  final totalsSheet = excel['Totaux'];
+
+  historySheet.appendRow([
+    TextCellValue('code'),
+    TextCellValue('designation'),
+    TextCellValue('barcode'),
+    TextCellValue('quantite'),
+    TextCellValue('date'),
+  ]);
+
+  final Map<String, int> totals = {};
+
+  for (final row in rows) {
+    historySheet.appendRow([
+      TextCellValue(row.code),
+      TextCellValue(row.designation),
+      TextCellValue(row.barcode),
+      IntCellValue(row.quantite),
+      TextCellValue(row.date.toIso8601String()),
+    ]);
+
+    totals[row.code] = (totals[row.code] ?? 0) + row.quantite;
+  }
+
+  totalsSheet.appendRow([
+    TextCellValue('code'),
+    TextCellValue('quantite_totale'),
+  ]);
+
+  totals.forEach((code, total) {
+    totalsSheet.appendRow([
+      TextCellValue(code),
+      IntCellValue(total),
+    ]);
+  });
+
+  final fileBytes = excel.encode();
+  if (fileBytes == null) {
+    throw Exception('Impossible de générer le fichier Excel.');
+  }
+
   final dir = await getApplicationDocumentsDirectory();
-  final file = File('${dir.path}/inventaire.xlsx');
-  file.writeAsBytesSync(fileBytes!);
+  final file = File('${dir.path}/inventaire_export.xlsx');
+
+  await file.writeAsBytes(fileBytes, flush: true);
+
+  return file;
 }
